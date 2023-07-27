c70c536979045bb14e50b274c414a4c0f1580072
danbez42 <100473177+danbez42@users.noreply.github.com>
2023-07-27 10:27:43 +0700

    Delete NewsPaper directory

--- NewsPaper/news/forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm
-from .models import Post
-
-
-class NewsForm(ModelForm):
-    class Meta:
-        model = Post
-        fields = ['author', 'title', 'text', 'categoryType']
